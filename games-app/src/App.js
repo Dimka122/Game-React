54ad88c5256d00dc4b1fed0d223500d622ed0499
Dimka122 <kassindima1@gmail.com>
2024-02-19 08:32:32 +0200

    Добавление Auth0 в приложение

--- games-app/src/App.js
+++ games-app/src/App.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'YOUR_AUTH0_DOMAIN', // домен
+  clientID: 'YOUR_AUTH0_CLIENT_ID', // клиент id
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
     const self = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
+
     setInterval(() => {
         self.props.moveObjects(self.canvasMousePosition);
     }, 10);
