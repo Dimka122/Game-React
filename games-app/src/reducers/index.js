242cbf923c6378b51050b9b8005e4f8f30d08515
Dimka122 <kassindima1@gmail.com>
2024-02-20 07:44:07 +0200

    добавление и изменения

--- games-app/src/reducers/index.js
+++ games-app/src/reducers/index.js
@@ -18,6 +18,16 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+      case LEADERBOARD_LOADED:
+        return {
+            ...state,
+            players: action.players,
+            };
+      case LOGGED_IN:
+        return {
+            ...state,
+            currentPlayer: action.player,
+            };
       case MOVE_OBJECTS:
         return moveObjects(state, action);
       case START_GAME:
